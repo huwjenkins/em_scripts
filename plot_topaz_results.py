d27e81c4a787bb93dc3676c914d06ff7444fc513
Huw Jenkins <huw.jenkins@york.ac.uk>
2023-12-06 11:07:42 +0000

    Retire Topaz training and picking results plotters

--- plot_topaz_results.py
+++ plot_topaz_results.py
@@ -1,93 +1,12 @@
 #! /usr/bin/env python
 # Plots histogram of Topaz picking scores from RELION autopick job
 # Author Huw Jenkins 205021
+# Replaced by plot_topaz.py 061223
 from __future__ import print_function
-import os
 import sys
-import argparse
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
-
-def read_headers(star_file):
-  got_labels = False
-  data = False
-  in_loop = False
-  with open(star_file) as f:
-    for line in f:
-      if line[0:5] == 'data_' and line.strip()[5:] in ['', 'coordinate_files']:
-        data = True
-        labels = []
-      elif data and line[0] == '_':
-        labels.append(line[line.find('_') + 1:line.find('#') - 1])
-      elif data and len(labels) > 0:
-        return labels
-      else:
-         continue
-
-def get_star_files(star_file):
-  star_files = []
-  labels=read_headers(star_file)
-  sf = labels.index('rlnMicrographCoordinates')
-  with open(star_file) as f:
-    data = False
-    for line in f:
-      if data and line.strip() != '':
-        items = line.split()
-        star_files.append(items[sf])
-      elif line.startswith('_' + labels[-1]):
-        data = True
-      else:
-        continue
-  return star_files
-
-def make_plot(star_file, output_file, min, max, bins):
-  if output_file == 'topaz_FOM.pdf': # default
-    output_file = os.path.join(os.path.split(star_file)[0], output_file)
-  foms = []
-  star_files = get_star_files(star_file)
-  for sf in star_files:
-    labels=read_headers(sf)
-    fom = labels.index('rlnAutopickFigureOfMerit')
-    with open(sf) as f:
-      data = False
-      for line in f:
-        if data and line.strip() != '':
-          items = line.split()
-          foms.append(float(items[fom]))
-        elif line.startswith('_' + labels[-1]):
-          data = True
-        else:
-          continue
-  print('Plotting histogram of FOM from {} picks from {} micrographs...'.format(len(foms), len(star_files)))
-  a = np.array(foms)
-  fig, ax1 = plt.subplots()
-  ax1.hist(a, bins=bins, range=(min, max))
-  ax1.set_xlabel('Predicted score (predicted log-likelihood ratio)')
-  ax1.set_ylabel('Number of particles')
-  ax1.xaxis.set_minor_locator(AutoMinorLocator())
-  plt.grid(True)
-  plt.savefig(output_file, format='pdf')
-  print('...written plot to {}'.format(output_file))
-  plt.close()
 
 if __name__=='__main__':
-  parser = argparse.ArgumentParser(description='Plot predicted scores from Topaz run through RELION')
-  parser.add_argument('star_file', metavar='Autopick/jobNNN/autopick.star', type=str,
-                      help='path from RELION job directory to star file from Autopick')
-  parser.add_argument('--output', required=False, default='topaz_FOM.pdf', metavar='topaz_FOM.pdf', type=str,
-                      help='output file_name')
-  parser.add_argument('--min', required=False, default=-6, metavar='-6', type=float,
-                      help='minimum score for plot')
-  parser.add_argument('--max', required=False, default=5, metavar='5', type=float,
-                      help='maximum score for plot')
-  parser.add_argument('--bins', required=False, default=50, metavar='50', type=int,
-                      help='number of bins in histogram')
-  args = parser.parse_args()
-  if not os.path.split(args.star_file)[0].startswith('AutoPick'):
-    sys.exit('Please run this script from the RELION job directory and supply the path to the AutoPick star file as Autopick/jobNNN/autopick.star')
-  if not os.path.split(args.star_file)[-1] == 'autopick.star':
-    sys.exit('Please run this script from the RELION job directory and supply the path to the AutoPick star file as Autopick/jobNNN/autopick.star')
-  if not os.path.isfile(args.star_file):
-    sys.exit('Could not find {}'.format(args.star_file))
-  make_plot(star_file=args.star_file, output_file=args.output, min=args.min, max=args.max, bins=args.bins)
+  print('This script has been deprecated in favour of the new plot_topaz.py script')
+  sys.exit('Please run plot_topaz.py from RELION job directory and supply the path to the star file as Autopick/jobNNN/job.star')
+
+
--- plot_topaz_training.py
+++ plot_topaz_training.py
@@ -1,48 +1,11 @@
 #! /usr/bin/env python
 # Plots progress of Topaz training when run from RELION
 # Author Huw Jenkins 235021
+# Replaced by plot_topaz.py 061223
 from __future__ import print_function
-import os
 import sys
-import argparse
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def make_plot(star_file, output_file):
-  if output_file == 'topaz_training.pdf': # default
-    output_file = os.path.join(os.path.split(star_file)[0], output_file)
-  with open(star_file) as f:
-    for line in f:
-      if line.strip() != '' and line[0] != '#':
-        if line.startswith('_rlnJobTypeLabel'):
-          job = line.split()[1].strip()
-        elif line.split()[0].strip() == 'topaz_nr_particles':
-          n = int(line.split()[1].strip())
-    if n < 0 or job != 'relion.autopick.topaz.train':
-      sys.exit("Sorry {} doesn't appear to be from a Topaz training job".format(star_file))
-  results_file = os.path.join(os.path.split(star_file)[0],'model_training.txt')
-  table = pd.read_csv(results_file, sep='\t')
-  table = table.loc[table['split'] == 'test'] # only keep the validation results
-  table['auprc'] = table['auprc'].astype(float)
-  print('Plotting area under the precision-recall curve for {} epochs of training...'.format(len(table)))
-  fig, ax = plt.subplots()
-  ax.plot(table['epoch'], table['auprc'], '-o',  label=str(n))
-  ax.set_xlabel('Epoch')
-  ax.set_ylabel('AUPRC')
-  ax.legend(loc='best')
-  plt.savefig(output_file, format='pdf')
-  print('...written plot to {}'.format(output_file))
-  plt.close()
 
 if __name__=='__main__':
-  parser = argparse.ArgumentParser(description='Plot progress of Topaz training run through RELION')
-  parser.add_argument('star_file', metavar='Autopick/jobNNN/job.star', type=str,
-                      help='path from RELION job directory to star file from Topaz training')
-  parser.add_argument('--output', required=False, default='topaz_training.pdf', metavar='topaz_training.pdf', type=str,
-                      help='output file_name')
-  args = parser.parse_args()
-  if not os.path.split(args.star_file)[0].startswith('AutoPick'):
-    sys.exit('Please run this script from the RELION job directory and supply the path to the star file as Autopick/jobNNN/job.star')
-  if not os.path.isfile(args.star_file):
-    sys.exit('Could not find {}'.format(args.star_file))
-  make_plot(star_file=args.star_file, output_file=args.output)
+  print('This script has been deprecated in favour of the new plot_topaz.py script')
+  sys.exit('Please run plot_topaz.py from RELION job directory and supply the path to the star file as Autopick/jobNNN/job.star')
+
